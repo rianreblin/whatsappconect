22ef5423fbac41a0cadc848a0f0eaf069cfa5de9
rianreblin <rianreblin6@gmail.com>
2025-09-04 13:01:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
+const puppeteer = require('puppeteer');
 
+// Cria a sessão do bot
 wppconnect.create({
   session: 'bot2',
   headless: true,
-  useChrome: true
+  useChrome: true,
+  browserArgs: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--disable-gpu'
+  ],
+  puppeteerOptions: {
+    executablePath: puppeteer.executablePath() // força usar o Chromium baixado pelo puppeteer
+  }
 })
 .then(client => start(client))
-.catch(err => console.log(err));
+.catch(err => console.log('Erro ao iniciar WPPConnect:', err));
 
 function start(client) {
+  console.log('🤖 Bot conectado e pronto!');
+
   client.onMessage(message => {
     if (message.body.toLowerCase() === 'oi' && !message.isGroupMsg) {
       client.sendText(message.from, 'Olá! Bot conectado no Render ✅');
